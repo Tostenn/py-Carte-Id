8fdd4c0f252c3f81c4d29c069ee52008e7928923
main <main@gmail.com>
2023-09-23 18:10:02 +0200

    ajout de la fonction __init__

--- main.py
+++ main.py
@@ -5,5 +5,11 @@ effter()
 
 class User:
 
-    def __init__(self) -> None:
+    def __init__(self,nom,prenom,sexe,taile,poids,job) -> None:
+        self.nom = nom
+        self.prenom = prenom
+        self.sex = sexe
+        self.taille = taile
+        self.poid = poids
+        self.job = job
         pass
