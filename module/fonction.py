e3fe33f33a5a7994ae5b725383570b3a07466c63
tosten <kouyatosten@gmail.com>
2023-10-07 01:52:06 +0200

    validation du commit et ajout de la section sauvegarde dans le readme.md

--- module/fonction.py
+++ module/fonction.py
@@ -1,31 +1,29 @@
-from os import system,path,mkdir
+
+from os import system,path,getcwd
 from time import sleep,localtime
 from sys import platform
 from json import load
+from repertoire import __ecri_fic__
+logo = lambda word = 'Py-Carte-ID' : f'\n{word:-^60}'
 
 # modules pour la creation et l'affichage d'image
 from PIL import Image, ImageDraw, ImageFont
 import numpy as np
 import cv2
 
+def rlt(x = 0.3) -> None:'''renlanti le programme'''; sleep(x)
 
-def rlt(x = 0.3) -> True:
-    # ralanti le programme
-    sleep(x)
-
-def effter():
-    # efface le terminal
-    system("cls") if platform == "win32" else system("clear")
+def effter() -> None:'''efface le terminal''';system("cls") if platform == "win32" else system("clear")
 
 def temps():
-    # renvoir la date et heure
+    '''renvoir la date et heure'''
     tp = localtime()
     an = [tp.tm_mday,tp.tm_mon,tp.tm_year]
     heur = [tp.tm_hour,tp.tm_min,tp.tm_sec]
     return an,heur
 
 def dataValidation(data:dict):
-    # validation des donnée
+    """validation des donnée"""
     data_error = {}
     for key, value in data.items():
 
@@ -77,14 +75,49 @@ def __conten_fic__(chemin,l = 'r') -> str:
             return file
         else : return 
 
+def __ecri_fic__(chemin,text,l = 'w'):
+    try:
+        with open(chemin,l,encoding='utf-8') as file:
+            f= file.write(text)
+    except:
+        with open(chemin,l) as file:
+            f= file.write(text)
+    return f
+    # renvoir le contenu d'un ficher
+
 def recujson(ch = 'data.json'):
-    # recupere les donners d'un fichier json
+    '''recupere les donners d'un fichier json'''
     try:
         if __veri_chemin__(ch) == 'ficher':
             with open(ch,'r',encoding='utf-8') as file:
                 data = load(file)
             return data
     except: return {}
+    
+def save(op_s:str,data:str):
+    """determine le format d'enregistre de la sortir"""
+
+    if not op_s:return
+    
+    action = True
+    if not op_s.endswith(('.txt',)):
+        action = False
+
+    # accepte les fichiers sans extensions
+    if not "." in op_s:
+        action = True
+    
+    if not action:
+        print(f'{logo()}\nle format du fichier indiquer n\'est pas pris en charge{logo()}')
+        return
+
+    if op_s.endswith('.txt') or not '.' in op_s:
+        saveTxt(op_s,data)
+
+def saveTxt(namefile:str,data:str):
+
+    __ecri_fic__(namefile,data)
+    print(f'{logo()}\nsauvegarde réussir | fichier {path.join(getcwd(),namefile)} {logo()}')
 
 def afficher_carte(carte):
     attrs = {
@@ -114,4 +147,4 @@ def afficher_carte(carte):
     cv2.imshow(carte.fmt_pays().upper() + carte.fmt_nb(), img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-    
+  
